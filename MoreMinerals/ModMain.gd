60e150a7e5fcb91124c7dc1dca6e6d399f780fd3
ZakrinYoran <145646699+ZakrinYoran@users.noreply.github.com>
2024-05-18 00:33:46 -0800

    MoreMinerals v0.5.0
    - Added Obonto trades for Si

--- MoreMinerals/ModMain.gd
+++ MoreMinerals/ModMain.gd
@@ -22,6 +22,9 @@ func _init(modLoader = ModLoader):
 	installScriptExtension("TheRing.gd")
 	installScriptExtension("CurrentGame.gd")
 
+	replaceScene("comms/conversation/HabitatConversation.tscn")
+
+	updateTL()
 
 	Debug.l(modName + ": Initialized")
 
@@ -29,6 +32,30 @@ func _init(modLoader = ModLoader):
 #Don't got nothin to do on ready
 func _ready():
 	Debug.l(modName + ": Ready")
+
+
+
+# Helper script to load translations, as the ModLoader one seemingly does not function.
+# This is not a good function, don't use it.
+func updateTL(path:String = modPath + "i18n/translation.txt"):
+	Debug.l(modName + ": Updating Translations")
+
+	var tlFile = File.new()
+	tlFile.open(path, File.READ)
+
+	var translation = Translation.new()
+	translation.locale = "en"
+
+	while tlFile.get_position() < tlFile.get_len():
+		var line = tlFile.get_line()
+		var split = line.split("|", false)
+		if split.size() == 2:
+			translation.add_message(split[0], tr(split[1]).c_unescape())
+			Debug.l(str("Added translation: ", split))
+
+	TranslationServer.add_translation(translation)
+
+	Debug.l(modName + ": Translations Updated")
 
 
 # Helper function to extend scripts
@@ -45,3 +72,22 @@ func installScriptExtension(path:String , oldPath:String = "none"):
 
 	childScript.take_over_path(parentPath)
 
+
+# Helper function to replace scenes
+func replaceScene(path:String, oldPath:String = "none"):
+	Debug.l("Updating scene: %s" % path)
+	var newScene
+	var oldScene
+
+	if oldPath == "none":
+		newScene = str(modPath + path)
+		oldScene = str("res://" + path)
+
+	else:
+		newScene = path
+		oldScene = oldPath
+
+	var scene = load(newScene)
+	scene.take_over_path(oldScene)
+	_savedObjects.append(scene)
+	Debug.l("Finished updating: %s" % oldScene)
