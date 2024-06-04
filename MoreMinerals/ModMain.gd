ae4dfdf6ad3d2f331e6bd0913835a761a750f3c8
ZakrinYoran <145646699+ZakrinYoran@users.noreply.github.com>
2024-06-04 15:58:14 -0800

    More Minerals v0.7.1
    - New function for adding translations

--- MoreMinerals/ModMain.gd
+++ MoreMinerals/ModMain.gd
@@ -7,64 +7,84 @@ var _savedObjects = []
 var modName : String = "MoreMinerals"
 var modPath : String
 
+
 #Initialize the mod
 func _init(modLoader = ModLoader):
 	Debug.l(modName + ": Initializing")
 
-	# Get current path of script
+#	Get current path of script
 	modPath = get_script().resource_path.get_base_dir() + "/"
 
-	#Install our script extensions
+#	Install our script extensions
+	initAsteroids()
+	initHUD()
+	initShip()
+	initElse()
+
+	Debug.l(modName + ": Initialized")
+
+
+func initAsteroids():
 	installScriptExtension("asteroids/mineral.gd")
 	installScriptExtension("AsteroidSpawner.gd")
 
+func initHUD():
 	installScriptExtension("hud/components/MineralLabel.gd")
 	installScriptExtension("hud/SystemMineralList.gd")
 
+func initShip():
 	installScriptExtension("ships/modules/MineralProcessingUnit.gd")
 	installScriptExtension("ships/ship-ctrl.gd")
 
+func initElse():
 	installScriptExtension("TheRing.gd")
 	installScriptExtension("CurrentGame.gd")
 
 	replaceScene("comms/conversation/HabitatConversation.tscn")
 
-	updateTL()
 
-	Debug.l(modName + ": Initialized")
-
-
-#Don't got nothin to do on ready
+#Do stuff on ready 
 func _ready():
+#	Add our translations
+	updateTL(modPath + "i18n/translation.txt", "|")
+
 	Debug.l(modName + ": Ready")
 
 
-
-# Helper script to load translations, as the ModLoader one seemingly does not function.
-# This is not a good function, don't use it.
-func updateTL(path:String = modPath + "i18n/translation.txt"):
-	Debug.l(modName + ": Updating Translations")
-
+# Helper script to load translations
+func updateTL(csvPath:String, delim:String = ","):
+	Debug.l("%s: Adding translations from: %s" % [modName, csvPath])
 	var tlFile = File.new()
-	tlFile.open(path, File.READ)
+	tlFile.open(csvPath, File.READ)
 
-	var translation = Translation.new()
-	translation.locale = "en"
+	var translations = []
 
-	while tlFile.get_position() < tlFile.get_len():
-		var line = tlFile.get_line()
-		var split = line.split("|", false)
-		if split.size() == 2:
-			translation.add_message(split[0], tr(split[1]).c_unescape())
-			Debug.l(str("Added translation: ", split))
+	var csvLine = tlFile.get_line().split(delim)
+	Debug.l("Adding translations as: %s" % csvLine)
+	for i in range(1, csvLine.size()):
+		var translationObject = Translation.new()
+		translationObject.locale = csvLine[i]
+		translations.append(translationObject)
 
-	TranslationServer.add_translation(translation)
+	while not tlFile.eof_reached():
+		csvLine = tlFile.get_csv_line(delim)
 
-	Debug.l(modName + ": Translations Updated")
+		if csvLine.size() > 1:
+			var translationID = csvLine[0]
+			for i in range(1, csvLine.size()):
+				translations[i - 1].add_message(translationID, csvLine[i].c_unescape())
+			Debug.l("Added translation: %s" % csvLine)
+
+	tlFile.close()
+
+	for translationObject in translations:
+		TranslationServer.add_translation(translationObject)
+
+	Debug.l("%s: Translations Updated" % modName)
 
 
 # Helper function to extend scripts
-func installScriptExtension(path:String , oldPath:String = "none"):
+func installScriptExtension(path:String):
 	var childPath = str(modPath + path)
 	var childScript = ResourceLoader.load(childPath)
 
